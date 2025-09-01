00f6d4b33f5a8ed5e0945393e817d8f7f13660f5
Valeria <valeria.velasco@tecsup.edu.pe>
2025-08-31 22:40:50 -0500

    Add two activitys and desing the connection beetween, them.

--- src/acuario/Acuario.kt
+++ src/acuario/Acuario.kt
@@ -7,7 +7,7 @@ open class Acuario(open var largo: Int = 100, open var ancho: Int = 20, open var
     }
 
     open var volumen: Int
-        get() = ancho * alto * largo / 1000  // 1000 cm^3 = 1 l
+        get() = ancho * alto * largo / 1000
         set(valor) {
             alto = (valor * 1000) / (ancho * largo)
         }
@@ -24,14 +24,12 @@ open class Acuario(open var largo: Int = 100, open var ancho: Int = 20, open var
     }
 
     constructor(numeroDePeces: Int) : this() {
-        // 2,000 cm^3 por pez + espacio extra para que no se derrame
         val tanque = numeroDePeces * 2000 * 1.1
         alto = (tanque / (largo * ancho)).toInt()
     }
 }
 class TanqueTorre (override var alto: Int, var diametro: Int): Acuario(alto = alto, ancho = diametro, largo = diametro) {
     override var volumen: Int
-        // área elíptica = π * r1 * r2
         get() = (ancho/2 * largo/2 * alto / 1000 * PI).toInt()
         set(valor) {
             alto = ((valor * 1000 / PI) / (ancho/2 * largo/2)).toInt()
